8ae6a243aea12ff00292459e537d191c753faebc
DESKTOP-V0K9MU2\user <bjs2417@inswave.com>
2022-08-02 18:00:43 +0900

    'test'

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="testBtn" @mousedown="handleMouseDown">testsetsetets </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import {ipcRenderer} from 'electron'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  setup() {
+    const handleMouseDown = () => {
+      console.log('ipcRenderer', ipcRenderer)
+      ipcRenderer.send('testFS','sucess!!!')
+      ipcRenderer.on('reciveFS', (evt, payload) => {
+        console.log('recive!!!', evt, payload)
+      })
+    }
+
+    return {
+      handleMouseDown
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
